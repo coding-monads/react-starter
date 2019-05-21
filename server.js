ac918fd64bf31c2cc6c4dc9427ee1bd5d3369f94
kamilmoskal <kamil_moskal@interia.pl>
2019-05-21 21:48:46 +0200

    Added User Model + register route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const test = require("./routes/api/test");
 
+const users = require("./routes/api/users");
+
 const app = express();
 
 // Init Middleware
@@ -13,6 +15,7 @@ connectDB();
 
 //Routes
 app.use("/api/test", test);
+app.use("/api/users", users);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
