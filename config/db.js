16a2babee97cd1c9e5ccd8b166670134ea81e471
kamilmoskal <45687747+kamilmoskal@users.noreply.github.com>
2019-05-29 21:15:30 +0200

    NT-0036 - Added optional env variables to change localhost in docker setting (#21)
    * Added optional env variables to change localhost in docker setting
    
    * fix mongo uri structure

--- config/db.js
+++ config/db.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const dbURI = config.get("mongo.uri");
+const { host, resource, query, name } = config.get("mongo.uri");
 const dbCredentials = config.get("mongo.credentials");
 
 const settings = {
-  ...dbCredentials,  
-  useNewUrlParser: true, 
-  useCreateIndex: true, 
-  useFindAndModify: false 
-}
+  ...dbCredentials,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+};
+
+const dbURI = `${name}://${host}/${resource}${query}`;
 
 const connectDB = () =>
-  console.log("Trying to connect to mongodb [URI] ", dbURI)
+  console.log("Trying to connect to mongodb [URI] ", dbURI);
 
-  mongoose
-    .connect(dbURI, settings)
-    .then(() => console.log("MoongoDB Connected"))
-    .catch(err => console.log("MoongoDB not connected", err));
+mongoose
+  .connect(dbURI, settings)
+  .then(() => console.log("MoongoDB Connected"))
+  .catch(err => console.log("MoongoDB not connected", err));
 
 module.exports = connectDB;
