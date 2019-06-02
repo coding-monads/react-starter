9f2d925169676667ef27e5f96f79a7ca7587ff42
Mateusz Kotula <Mateusz.kotula@op.pl>
2019-06-02 19:42:22 +0200

    Feature/nt 0037 - supertest (#25)
    * create variables that are used for testing
    
    * separate data to make tests work properly
    
    * experimental tests

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 require("dotenv").config();
+const app = require('./app');
 
-const express = require("express");
-const connectDB = require("./config/db");
-const passport = require("passport");
-const test = require("./routes/api/test");
-
-const users = require("./routes/api/users");
-
-const app = express();
-
-// Init Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Connect Database
+const {connectDB}= require("./config/db");
 connectDB();
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport Config
-require("./config/passport")(passport);
-
-// Routes
-app.use("/api/test", test);
-app.use("/api/users", users);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
